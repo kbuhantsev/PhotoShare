f10c8bcdf12bd1fa94acabeb04e619ad8295e5ef
tarsweb <tarsweb@ukr.net>
2024-05-16 10:54:05 +0300

    for test

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from src.settings import settings
 from src.database import Base
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -70,9 +71,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
