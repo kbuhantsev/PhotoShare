b720dab62e697605ed6575e17c0c2b3dc0ed0ff0
kbuhantsev <k.buhantsev@gmail.com>
2024-05-16 10:20:14 +0300

    alembic config

--- src/database.py
+++ src/database.py
@@ -2,6 +2,20 @@ from sqlalchemy import Integer, func, DateTime
 
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from settings import settings
+
+engine = create_async_engine(settings.get_db_uri())
+async_session_factory = async_sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+
+
+# Dependency
+async def get_db():
+    async with async_session_factory() as session:
+        yield session
+
 
 class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
