98c137cdf96ed05051a49a1d7ff9ba5ebb835553
tarsweb <tarsweb@ukr.net>
2024-05-16 14:24:23 +0300

    update healthchecker

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import text
+
+from fastapi import FastAPI, Depends, status, HTTPException
+
+from src.database import get_db
 
 from src.auth.router import router as auth_router
 from src.photos.router import router as photos_router
@@ -21,5 +26,19 @@ async def root():
 
 
 @app.get("/api/healthchecker")
-async def healthchecker():
-    return {"message": "Welcome to FastAPI"}
+async def healthchecker(db: AsyncSession = Depends(get_db)):
+    try:
+        # Make request
+        result = await db.execute(text("SELECT 1"))
+        result = result.fetchone()
+        if result is None:
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly"
+            )
+        return {"message": "Welcome to FastAPI"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error connecting to the database",
+        )
