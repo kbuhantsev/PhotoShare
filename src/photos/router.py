fe3d9683314697be5403873c5f0bc29ba1f57d46
kbuhantsev <k.buhantsev@gmail.com>
2024-05-17 11:24:24 +0300

    handlers

--- src/photos/router.py
+++ src/photos/router.py
@@ -4,7 +4,8 @@ from typing import Annotated
 from fastapi import File, Form, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.photos.service import create_photo
+from src.auth.dependencies import get_current_user
+from src.photos.service import create_photo, update_photo, get_photo, delete_photo
 from src.database import get_db
 from src.photos.schemas import PhotoResponseSchema
 from src.user.models import User
@@ -15,14 +16,45 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def get_photos():
-    return {"message": "Hello World"}
+@router.get("/", response_model=PhotoResponseSchema, status_code=status.HTTP_200_OK)
+async def get_photos(
+    skip: int = 0, limit: int = 20, db: AsyncSession = Depends(get_db)
+):
+
+    response = PhotoResponseSchema()
+
+    try:
+        photos = await get_photos(skip=skip, limit=limit, db=db)
+        response.data = photos
+        return response
+    except Exception as e:
+        response.status = "error"
+        response.message = "An error occurred while getting the photos!"
+        return response
 
 
-@router.get("/{photo_id}")
-async def get_photo(photo_id: int):
-    return {"message": "Hello World"}
+@router.get(
+    "/{photo_id}", response_model=PhotoResponseSchema, status_code=status.HTTP_200_OK
+)
+async def get_photo_by_id(photo_id: int, db: AsyncSession = Depends(get_db)):
+
+    response = PhotoResponseSchema()
+
+    try:
+        photo = await get_photo(photo_id=photo_id, db=db)
+    except Exception as e:
+        response.status = "error"
+        response.message = "An error occurred while getting the photo!"
+        return response
+
+    if not photo:
+        response.status = "error"
+        response.message = "An error occurred while getting the photo!"
+        return response
+
+    response = PhotoResponseSchema()
+    response.data = photo
+    return response
 
 
 @router.post(
@@ -34,19 +66,24 @@ async def create_photo(
     description: Annotated[str, Form()],
     tags: Annotated[list[str], Form()],
     db: AsyncSession = Depends(get_db),
-    current_user: User = Depends(),
+    current_user: User = Depends(get_current_user),
 ):
 
-    photo = await create_photo(
-        title=title,
-        file=file.file,
-        description=description,
-        tags=tags,
-        db=db,
-        current_user=current_user,
-    )
-
     response = PhotoResponseSchema()
+
+    try:
+        photo = await create_photo(
+            title=title,
+            file=file.file,
+            description=description,
+            tags=tags,
+            db=db,
+            current_user=current_user,
+        )
+    except Exception as e:
+        response.status = "error"
+        response.message = "An error occurred while creating the photo!"
+        return response
 
     if not photo:
         response.status = "error"
@@ -58,11 +95,63 @@ async def create_photo(
     return response
 
 
-@router.put("/{photo_id}")
-async def update_photo(photo_id: int):
-    return {"message": "Hello World"}
+@router.put(
+    "/{photo_id}", status_code=status.HTTP_200_OK, response_model=PhotoResponseSchema
+)
+async def update_photo_by_id(
+    photo_id: int,
+    title: Annotated[str, Form()],
+    file: Annotated[UploadFile, File()],
+    description: Annotated[str, Form()],
+    tags: Annotated[list[str], Form()],
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+
+    response = PhotoResponseSchema()
+
+    try:
+        photo = await update_photo(
+            photo_id=photo_id,
+            title=title,
+            file=file.file,
+            description=description,
+            tags=tags,
+            db=db,
+        )
+    except Exception as e:
+        response.status = "error"
+        response.message = "An error occurred while updating the photo!"
+        return response
+
+    if not photo:
+        response.status = "error"
+        response.message = "An error occurred while updating the photo!"
+        return response
+
+    response = PhotoResponseSchema()
+    response.data = photo
+    return response
 
 
-@router.delete("/{photo_id}")
-async def delete_photo(photo_id: int):
-    return {"message": "Hello World"}
+@router.delete(
+    "/{photo_id}", response_model=PhotoResponseSchema, status_code=status.HTTP_200_OK
+)
+async def delete_photo_by_id(photo_id: int, db: AsyncSession = Depends(get_db)):
+
+    response = PhotoResponseSchema()
+
+    try:
+        photo = await delete_photo(photo_id=photo_id, db=db)
+    except Exception as e:
+        response.status = "error"
+        response.message = "An error occurred while deleting the photo!"
+        return response
+
+    if not photo:
+        response.status = "error"
+        response.message = "An error occurred while deleting the photo!"
+        return response
+
+    response = PhotoResponseSchema()
+    return response
--- src/photos/service.py
+++ src/photos/service.py
@@ -1,32 +1,89 @@
 from typing import BinaryIO
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
+from src.photos.models import Photo
+from src.photos.utils import upload_file, delete_file
+from src.tags.models import Tag
 from src.user.models import User
 
 
 async def create_photo(
-    *,
-    title: str,
-    file: BinaryIO,
-    description: str,
-    tags: list[str],
-    db: AsyncSession,
-    current_user: User
-):
-    pass
+        *,
+        title: str,
+        file: BinaryIO,
+        description: str,
+        tags: list[str],
+        db: AsyncSession,
+        current_user: User
+) -> Photo | None:
+    asset = await upload_file(file, folder="photos")
+    photo = Photo(
+        title=title,
+        description=description,
+        owner_id=current_user.id,
+        public_id=asset.get("public_id"),
+        secure_url=asset.get("secure_url"),
+        folder="photos",
+        tags=[Tag(name=tag) for tag in tags],
+    )
+    db.add(photo)
+    await db.commit()
+    await db.refresh(photo)
+    return photo
 
 
-async def update_photo(*, photo_id: int):
-    pass
+async def update_photo(
+        *,
+        photo_id: int,
+        title: str,
+        file: BinaryIO,
+        description: str,
+        tags: list[str],
+        db: AsyncSession) -> Photo | None:
+
+    query = select(Photo).where(Photo.id == photo_id)
+    res = await db.execute(query)
+    photo = res.scalars().first()
+    if not photo:
+        return None
+
+    asset = await upload_file(file, folder="photos")
+    photo.title = title
+    photo.description = description
+    photo.public_id = asset.get("public_id")
+    photo.secure_url = asset.get("secure_url")
+    photo.folder = "photos"
+    photo.tags = [Tag(name=tag) for tag in tags]
+    await db.commit()
+    await db.refresh(photo)
+    return photo
 
 
-async def delete_photo(*, photo_id: int):
-    pass
+async def delete_photo(*, photo_id: int, db: AsyncSession) -> Photo | None:
+    query = select(Photo).where(Photo.id == photo_id)
+    res = await db.execute(query)
+    photo = res.scalars().one_or_none()
+    if not photo:
+        return None
+
+    deleted = await delete_file(public_id=photo.public_id)
+    if not deleted:
+        return None
+
+    await db.delete(photo)
+    await db.commit()
+
+    return photo
 
 
-async def get_photos():
-    pass
+async def get_photos(skip: int, limit: int, db: AsyncSession) -> list[Photo]:
+    query = select(Photo).offset(skip).limit(limit)
+    res = await db.execute(query)
+    return list(res.scalars().all())
 
 
-async def get_photo(*, photo_id: int):
-    pass
+async def get_photo(*, photo_id: int, db: AsyncSession) -> Photo | None:
+    query = select(Photo).where(Photo.id == photo_id)
+    res = await db.execute(query)
+    return res.scalars().one_or_none()
