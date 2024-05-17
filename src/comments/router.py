d136cd3b7f4ffe4e6da1416e1bd02487e9a48abc
LuytensStar <artem05zhar@gmail.com>
2024-05-17 16:03:19 +0200

    separate logic in service.py , correct Session to AsyncSession, fixed CommentModel

--- src/comments/router.py
+++ src/comments/router.py
@@ -1,13 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException,status
-from sqlalchemy.orm import Session
-from sqlalchemy import and_
+from fastapi import APIRouter, Depends
 from src.database import get_db
-from src.comments.models import Comment
-from datetime import datetime
 from src.comments.schemas import CommentModel, CommentCreate, CommentUpdate
-from typing import List, Union
+from typing import List
 from src.auth.dependencies import get_current_user
 from src.user.models import User
+from sqlalchemy.ext.asyncio import AsyncSession
+import src.comments.service as comment_services
+
 router = APIRouter(
     prefix="/comments",
     tags=["comments"],
@@ -17,55 +16,30 @@ router = APIRouter(
 
 #TODO Додати обмеження на кількіть запитів
 @router.post("/", response_model=CommentModel, description="No more than 15 requests per minute")
-async def create_comment(photo_id:int, comment: CommentCreate, db: Session = Depends(get_db),
+async def create_comment(photo_id:int, comment: CommentCreate, db:AsyncSession= Depends(get_db),
                          current_user : User = Depends(get_current_user)):
-    try:
-        db_comment = Comment(comment=comment, photo_id=photo_id, user_id=current_user.id)
-        db.add(db_comment)
-        db.commit()
-        db.refresh(db_comment)
-        return {"status": 'ok', "message":" ", "data": comment}
 
-    except Exception as e:
-        return {"status": 'error', "message":e, "data": comment}
+    comment_create = await comment_services.create_comment(photo_id=photo_id, comment=comment, db=db, current_user=current_user)
+    return comment_create
 
 @router.get('/{photo_id}', response_model=List[CommentModel])
-async def get_comments(photo_id: int, db:Session = Depends(get_db),
-                       current_user : User = Depends(get_current_user)):
-    try:
-        comments = await db.query(Comment).filter(Comment.photo_id == photo_id).all()
-        return {"status": 'ok', "message":" ", "data": [comment for comment in comments]}
-    except Exception as e:
-        return {"status": 'error', "message":e, "data": " "}
+async def get_comments(photo_id: int, db: AsyncSession = Depends(get_db)):
+    comment_get = await comment_services.get_comments(photo_id=photo_id, db= db)
+    return comment_get
+
 
 @router.put("/{comment_id}", response_model=CommentModel)
-async def update_comment(comment_id: int, comment: CommentUpdate, db: Session = Depends(get_db),
+async def update_comment(comment_id: int, comment: CommentUpdate, db: AsyncSession = Depends(get_db),
                          current_user : User = Depends(get_current_user)):
-    try:
-        db_comment = db.query(Comment).filter(and_(Comment.id == comment_id, Comment.user_id == current_user.id))
-        if not db_comment:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-        db_comment.comment = comment.comment
-        db_comment.updated_at = datetime.utcnow()
-        db.commit()
-        db.refresh(db_comment)
-        return {"status": 'ok', "message":" ", "data": comment}
 
-    except Exception as e:
-        return {"status": 'error', "message":e, "data": comment}
+    updated_comment = comment_services.update_comment(comment_id=comment_id, comment=comment,
+                                                      db=db,current_user=current_user)
+    return updated_comment
+
 
 @router.delete('/{comment_id}', response_model=CommentModel)
-async def delete_comment(comment_id: int, db: Session = Depends(get_db),
+async def delete_comment(comment_id: int, db: AsyncSession = Depends(get_db),
                          current_user : User = Depends(get_current_user)):
-    try:
-        if current_user.role != 1 and current_user.role != 2:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                                detail="Only administrators and moderators can delete comments")
-        db_comment = db.query(Comment).filter(Comment.id == comment_id)
-        if not db_comment:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-        db.delete(db_comment)
-        db.commit()
-        return {"status": 'ok', "message":" ", "data": None}
-    except Exception as e:
-        return {"status": 'error', "message": e, "data": None}
+
+    deleted_comment = comment_services.delete_comment(comment_id=comment_id, db=db, current_user=current_user)
+    return deleted_comment
