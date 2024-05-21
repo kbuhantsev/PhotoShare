c883b5ecf688ed76902641c00ad17e298a50c971
dmitriy <kylishovdimka@ukr.net>
2024-05-22 00:09:41 +0300

    fix /tags --tags_all

--- src/tags/router.py
+++ src/tags/router.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from typing import List
-from sqlalchemy.exc import IntegrityError
-from src.tags.schemas import TagCreate, TagResponse
+from src.tags.schemas import TagResponse
 from src.database import get_db
 from src.tags.models import Tag
 
@@ -14,8 +13,8 @@ router = APIRouter(
 )
 
 
-@router.get("/tags", response_model=List[TagResponse])
-async def get_tags(db: AsyncSession = Depends(get_db)):
+@router.get("/tags_all", response_model=List[TagResponse])
+async def get_tags_all(db: AsyncSession = Depends(get_db)):
     async with db.begin():
         result = await db.execute(select(Tag))
         tags = result.scalars().all()
--- src/tags/schemas.py
+++ src/tags/schemas.py
@@ -2,10 +2,6 @@
 from pydantic import BaseModel, Field
 
 
-class TagCreate(BaseModel):
-    name: str = Field(..., min_length=3, max_length=25)
-
-
 class TagResponse(BaseModel):
     id: int
     name: str
