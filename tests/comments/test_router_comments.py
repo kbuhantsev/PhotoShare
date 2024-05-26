ddda018454dcf6dfd2b6b690c8bf2be08ec79045
Kostiantyn Buchantsev <k.buhantsev@gmail.com>
2024-05-26 14:34:01 +0300

    tests

--- tests/comments/test_router_comments.py
+++ tests/comments/test_router_comments.py
@@ -54,13 +54,6 @@ class TestGetCommentsHandler(unittest.IsolatedAsyncioTestCase):
         mock_get_comments.assert_called_once_with(photo_id=1, db=self.mock_db)
 
 
-class TestUpdateCommentHandler(unittest.IsolatedAsyncioTestCase):
-
-    def setUp(self):
-        self.mock_db = AsyncMock(spec=AsyncSession)
-        self.mock_comment = CommentResponseSchema(id=1, content="Updated comment")
-        self.mock_user = User(id=1, username="testuser", email="test@example.com")
-
     @patch("src.comments.service.update_comment")
     @patch("src.dependencies.get_current_user")
     @patch("src.database.get_db")
@@ -75,13 +68,6 @@ class TestUpdateCommentHandler(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(response.json(), {"data": self.mock_comment.dict()})
         mock_update_comment.assert_called_once_with(comment_id=1, comment="Updated comment", db=self.mock_db,
                                                     current_user=self.mock_user)
-
-
-class TestDeleteCommentHandler(unittest.IsolatedAsyncioTestCase):
-
-    def setUp(self):
-        self.mock_db = AsyncMock(spec=AsyncSession)
-        self.mock_comment = CommentResponseSchema(id=1, content="Test comment")
 
     @patch("src.comments.service.delete_comment")
     @patch("src.database.get_db")
