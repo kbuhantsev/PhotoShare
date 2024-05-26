106b27fa4f0f2834e4ac945ba28c664a6309fa7e
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-05-26 20:34:22 +0300

    tests for user service

--- tests/user/test_service_user.py
+++ tests/user/test_service_user.py
@@ -8,8 +8,22 @@ from src.user.models import User, Role
 from src.photos.models import Photo
 from src.comments.models import Comment
 from src.tags.models import Tag
-from src.user.service import create_user, get_count_users, get_user_by_email
-from src.user.schemas import UserSchema
+from src.user.service import (
+    create_user,
+    get_count_users,
+    get_user_by_email,
+    update_user,
+    update_avatar_url,
+    update_password,
+    update_role,
+    update_token,
+    block_user,
+    get_all_users,
+    get_user_profile,
+    get_user_comments,
+    get_user_photos,
+)
+from src.user.schemas import UserSchema, UserUpdateSchema
 
 
 class TestUserService(unittest.IsolatedAsyncioTestCase):
@@ -25,21 +39,33 @@ class TestUserService(unittest.IsolatedAsyncioTestCase):
         self.assertIsInstance(result, int)
         self.assertEqual(result, 0)
 
-    # @patch("src.user.service.get_count_users")
-    async def test_create_user(self):
+    @patch("src.user.service.get_count_users", new=AsyncMock(return_value=0))
+    async def test_create_first_user(self):
+
         body = UserSchema(
             email="test@example.com", username="test", password="123456789"
         )
 
-        get_count_users =MagicMock(return_value=0)
-        
-        result = await create_user(
-            body=body,
-            db=self.session,
-        )
+        result = await create_user(body=body, db=self.session)
 
         self.assertIsInstance(result, User)
         self.assertEqual(result.email, body.email)
+        self.assertEqual(result.username, body.username)
+        self.assertEqual(result.role, Role.ADMIN)
+
+    @patch("src.user.service.get_count_users", new=AsyncMock(return_value=1))
+    async def test_create_next_user(self):
+
+        body = UserSchema(
+            email="test@example.com", username="test", password="123456789"
+        )
+
+        result = await create_user(body=body, db=self.session)
+
+        self.assertIsInstance(result, User)
+        self.assertEqual(result.email, body.email)
+        self.assertEqual(result.username, body.username)
+        self.assertEqual(result.role, None)
 
     async def test_get_user_by_email(self):
         user = User(
@@ -57,9 +83,127 @@ class TestUserService(unittest.IsolatedAsyncioTestCase):
         self.assertIsInstance(result, User)
         self.assertEqual(result.email, user.email)
 
-    # async def test_get_user_by_id(self):
-    #     user_id = 1
-    #     result = await users.get_user_by_id(user_id=user_id, db=self.session)
-    #     self.assertIsInstance(result, User)
+    async def test_get_user_by_email_not_found(self):
+        user = User(
+            id=1,
+            username="test",
+            email="test@example.com",
+            password="testtest",
+            avatar=None,
+            role="USER",
+        )
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = None
+        self.session.execute.return_value = mocked_user
+        result = await get_user_by_email(email=user.email, db=self.session)
+        self.assertIsNone(result)
 
-    # async def test_get_current_user(self):
+    async def test_update_user(self):
+        user = User()
+        user_fields = UserUpdateSchema(username="test2", email="test2@example.com")
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await update_user(email=user.email, body=user_fields, db=self.session)
+        self.assertIsInstance(result, User)
+        self.assertEqual(result.email, user_fields.email)
+        self.assertEqual(result.username, user_fields.username)
+
+    async def test_update_avatar_url(self):
+        user = User()
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await update_avatar_url(
+            email=user.email, url="test_url", db=self.session
+        )
+        self.assertIsInstance(result, User)
+        self.assertEqual(result.avatar, "test_url")
+
+    async def test_update_password(self):
+        user = User()
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await update_password(
+            email=user.email, password="test_password", db=self.session
+        )
+        self.assertIsInstance(result, User)
+        self.assertEqual(result.password, "test_password")
+
+    async def test_update_role(self):
+        user = User()
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await update_role(user=user, role=Role.ADMIN, db=self.session)
+        self.assertIsInstance(result, User)
+        self.assertEqual(result.role, Role.ADMIN)
+
+    async def test_update_token(self):
+        user = User()
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await update_token(user=user, token="test_token", db=self.session)
+        self.assertEqual(result, None)
+        self.assertEqual(user.refresh_token, "test_token")
+
+    async def test_block_user(self):
+        user = User()
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await block_user(user=user, block=True, db=self.session)
+        self.assertIsInstance(result, User)
+        self.assertEqual(result.blocked, True)
+
+    async def test_unblock_user(self):
+        user = User()
+        mocked_user = MagicMock()
+        mocked_user.scalar_one_or_none.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await block_user(user=user, block=False, db=self.session)
+        self.assertIsInstance(result, User)
+        self.assertEqual(result.blocked, False)
+
+    async def test_get_all_users(self):
+        users_data =[{"user": User()}, {"user": User()}]
+        mocked_users = MagicMock()
+        mocked_users.mappings.return_value.all.return_value = users_data
+        self.session.execute.return_value = mocked_users
+        result = await get_all_users(db=self.session)
+        self.assertIsInstance(result, list)
+        self.assertEqual(result, [user_data["user"].to_dict() for user_data in users_data])
+
+    async def test_get_user_profile(self):
+        user = {"user": User()}
+        mocked_user = MagicMock()
+        mocked_user.mappings.return_value.first.return_value = user
+        self.session.execute.return_value = mocked_user
+        result = await get_user_profile(username=user["user"].username, db=self.session)
+        self.assertIsInstance(result, dict)
+        self.assertEqual(result, user["user"].to_dict())
+    
+    async def test_get_user_profile_not_found(self):
+        mocked_user = MagicMock()
+        mocked_user.mappings.return_value.first.return_value = None
+        self.session.execute.return_value = mocked_user
+        result = await get_user_profile(username="", db=self.session)
+        self.assertEqual(result, None)
+
+    async def test_get_user_photos(self):
+        photos = [Photo(), Photo(), Photo()]
+        mocked_photos = MagicMock()
+        mocked_photos.scalars.return_value.all.return_value = photos
+        self.session.execute.return_value = mocked_photos
+        result = await get_user_photos(skip=0, limit=50, user= User(), db=self.session)
+        self.assertEqual(result, photos)
+
+    async def test_get_user_comments(self):
+        comments = [Comment(), Comment(), Comment()]
+        mocked_comments = MagicMock()
+        mocked_comments.scalars.return_value.all.return_value = comments
+        self.session.execute.return_value = mocked_comments
+        result = await get_user_comments(skip=0, limit=50, user= User(), db=self.session)
+        self.assertEqual(result, comments)
